a8df0e11454063faba13403a6b2c40cd7a66850e
alsu Bulatova <bulatovaalsu22@gmail.com>
2022-11-18 00:24:04 +0500

    added color change from blue to red

--- Test task/index.js
+++ Test task/index.js
@@ -7,25 +7,32 @@ const createTimerAnimator = () => {
   let min;
   let hours;
   let remainingTime;
-  return (seconds) => {
+  let deltaColor = 0;
+  return (seconds, delta) => {
     let timer = setInterval(function () {
-        sec = seconds % 60 // Получаем секунды
-        min = seconds / 60 % 60 // Получаем минуты
-        hours = seconds / 60 / 60 % 60 // Получаем часы
-      if (seconds <= 0) {
-        clearInterval(timer);
-        alert("Время закончилось");
-      }
-      else {
+        sec = seconds % 60
+        min = seconds / 60 % 60
+        hours = seconds / 60 / 60 % 60
+
         remainingTime = `<div class="number center">${Math.trunc(hours)}</div>
                             <div class="colon center">:</div>
                             <div class="number center">${Math.trunc(min)}</div>
                             <div class="colon center">:</div>
                             <div class="number center">${sec}</div>`;
         timerEl.innerHTML = remainingTime;
+      if (seconds <= 0) {
+        clearInterval(timer);
+        // alert("Время закончилось");
       }
       --seconds;
-    }, 1000)
+    }, 1000);
+    let colorChangeTimer = setInterval(function () {
+          colorChange(deltaColor);
+          deltaColor++;
+          if (deltaColor === 120) {
+              clearInterval(colorChangeTimer);
+          }
+      }, delta/120*1000);
   };
 };
 
@@ -39,8 +46,15 @@ inputEl.addEventListener('input', () => {
 buttonEl.addEventListener('click', () => {
   console.log(inputEl.value);
   const seconds = Number(inputEl.value);
+  const delta = seconds;
 
-  animateTimer(seconds);
+  animateTimer(seconds, delta);
 
   inputEl.value = ``;
 });
+
+const colorChange = (num) => {
+    document.documentElement.style.setProperty('--main-color', `hsl(${240+num}, 79%, 66%)`);
+    document.documentElement.style.setProperty('--light-shadow', `hsl(${240+num}, 70%, 73%)`);
+    document.documentElement.style.setProperty('--dark-shadow', `hsl(${240+num}, 57%, 53%)`);
+}
